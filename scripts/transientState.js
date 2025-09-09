ccd6b163a46e1cbd5487b24b29ad1d295d0384fd
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-08 20:31:31 -0500

    Added form completion validation

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -12,6 +12,8 @@ export const setSocioLocationId = (chosenLocation) => {
 }
 
 export const savesurveySubmission = async () => {
+
+    if (transientState.socioLocationId > 0 && typeof(transientState.ownsBlueJeans) === 'boolean') {
     const postOptions = {
         method: "POST",
         headers: {
@@ -19,5 +21,7 @@ export const savesurveySubmission = async () => {
         },
         body: JSON.stringify(transientState)
     }
-    const response = await fetch("http://localhost:8088/submissions", postOptions)
+    const response = await fetch("http://localhost:8088/submissions", postOptions)} else {
+        window.alert("Please complete the form")
+    }
 }
