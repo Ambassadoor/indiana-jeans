ff83f7e9b05ec6229f226757d2179e417cdb5a36
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-08 19:14:50 -0500

    Created basic LocationChoices component

---
+++ scripts/LocationChoices.js
@@ -0,0 +1,18 @@
+export const LocationChoices = () => {
+    // TODO: Fetch locations from the API
+    
+
+    let html = `
+        <div class="survey-input">
+            <h2>What type of area do you live in?</h2>
+        </div>
+    `
+
+    // TODO: generate radio buttons and add to html
+
+    html += `
+        </div>
+    `
+
+    return html
+}
