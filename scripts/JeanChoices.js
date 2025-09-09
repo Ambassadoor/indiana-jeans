f6188aed9e8a21dde19364eb571f727466879caf
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-08 19:31:52 -0500

    added owernship event handler

--- scripts/JeanChoices.js
+++ scripts/JeanChoices.js
@@ -1,6 +1,18 @@
+import { setOwnBlueJeans } from "./transientState.js"
+
+const handleOwnershipChange = (changeEvent) => {
+    if (changeEvent.target.name === "ownJeans") {
+        const convertedToBoolean = JSON.parse(changeEvent.target.value)
+        setOwnBlueJeans(convertedToBoolean)
+    }
+}
+
 export const JeanChoices = () => {
+
+    document.addEventListener("change", handleOwnershipChange)
+
     let html = `
-    <div class="survey-input">
+    <div class="survey-input id="jeans-choice">
         <h2>Do you own a pair of blue jeans?</h2>
         <input type="radio" name="ownsJeans" value="true" /> Yes
         <input type="radio" name="ownsJeans" value="false"/> No
