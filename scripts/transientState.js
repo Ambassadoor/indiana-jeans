0c35ea2ea31c1560afb2eaa53fc83f347cb785d3
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-09-20 21:17:32 -0500

    converted transient state to map

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -1,36 +1,37 @@
-const transientState = {
-    ownsBlueJeans: false,
-    socioLocationId: 0,
-    brandName: "",
-    yearOfPurchase: 0
-}
+const transientState = new Map()
+
+// const transientState = {
+//     ownsBlueJeans: false,
+//     socioLocationId: 0,
+//     brandName: "",
+//     yearOfPurchase: 0
+// }
 
 export const setOwnBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership
+    transientState.set("ownsBlueJeans", chosenOwnership)
 }
 
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation
+    transientState.set("socioLocationId", chosenLocation)
 }
 
 export const setBrandName = (brand) => {
-    console.log(brand)
-    transientState.brandName = brand
+    transientState.set("brandName", brand)
 }
 
 export const setYearOfPurchase = (year) => {
-    transientState.yearOfPurchase = year
+    transientState.set("yearOfPurchase", year)
 }
 
 export const saveSurveySubmission = async () => {
-    console.log(transientState)
-    if (transientState.socioLocationId > 0 && typeof(transientState.ownsBlueJeans) === 'boolean') {
+
+    if (transientState.get("socioLocationId") > 0 && typeof(transientState.get("ownsBlueJeans")) === 'boolean') {
     const postOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(Object.fromEntries(transientState))
     }
     const response = await fetch("http://localhost:8088/submissions", postOptions)
     const newSubmissionEvent = new CustomEvent("newSubmissionCreated")
